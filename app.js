3359fb4a1c7e330f94d0eab50a21dfb609182b57
Aman Johar <amanjohar@Amans-MacBook-Pro.local>
2017-10-17 18:27:21 +0800

    here

--- app.js
+++ app.js
@@ -4,27 +4,29 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs=require('express-handlebars');
-var flash=require('connect-flash');
-var mongo=require('mongodb');
-var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/elearn');
-var db= mongoose.connection;
-var expressValidator=require('express-validator');
-var bcrypt=require('bcryptjs');
-var session =require('express-session');
-var passport=require('passport');
-var LocalStrategy=require('passport-local').Strategy;
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://amanjohar:amanjohar@ds149124.mlab.com:49124/amandb');
+var db = mongoose.connection;
+async = require('async');
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 var classes = require('./routes/classes');
+var students = require('./routes/students');
+var instructors = require('./routes/instructors');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars',exphbs({defaultLayout:'layout'}));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
@@ -35,44 +37,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//express-session
+// Express Session
 app.use(session({
-	secret:'secret',
-	saveUninitialized:true,
-	resave:true
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
 
-//passport
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//express-validator
+
+// Express Validator
 app.use(expressValidator({
-	errorFormatter: function(param, msg, value){
-		var namespace=param.split('.')
-		, root = namespace.shift()
-		, formParam=root;
-		while(namespace.length){
-			formParam += '['+namespace.shift()+']';
-		}
-		return{
-			param : formParam,
-			msg : msg,
-			value :value
-		};
-	}
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
 }));
 
-//connect-flash express-messages
+// Connect-Flash
 app.use(flash());
+
+app.get('*', function(req,res,next){
+	res.locals.user=req.user || null;
+	if(req.user){
+		res.locals.type=req.user.type;
+	}
+	next();
+})
+
+// Global Vars
 app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
+  res.locals.success_msg= req.flash('success_msg');
+	res.locals.error_msg= req.flash('error_msg');
+	res.locals.error= req.flash('error');
+
   next();
 });
 
-app.use('/', index);
+
+app.use('/', routes);
 app.use('/users', users);
 app.use('/classes', classes);
+app.use('/students',students);
+app.use('/instructors',instructors);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -81,15 +102,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
